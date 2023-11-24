c3f72a06a559f0da7020163c2a0f123633f6882e
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-11-24 23:36:09 +0600

    [DONE] 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,8 @@
-
-
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className='flex flex-col items-center justify-center my-auto h-[100vh]'>
+            <h3 className='font-bold text-8xl text-secondary'>404</h3>
+            <h4 className='font-semibold text-7xl text-accent'>NOT FOUND</h4>
         </div>
     );
 };
